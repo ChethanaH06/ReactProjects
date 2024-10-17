810e48c03dd53a54f81bfbf74e398bbe42cb1b84
ChethanaH06 <114866980+ChethanaH06@users.noreply.github.com>
2024-10-17 11:55:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Intro from "./Components/Intro/Intro";
+import NavBar from "./Components/NavBar/NavBar";
+import Skills from "./Components/Skills/Skills";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Intro/>
+      <Skills/>
+    </div>
+  );
+}
+
+export default App;
